6ced165ca8022e17ddbe3019da5c0abbb882001c
yeonju <llyyjj292@gmail.com>
2023-09-16 10:46:50 +0900

    feat: react router dom
    Signed-off-by: yeonju <llyyjj292@gmail.com>

--- dolgol/src/App.js
+++ dolgol/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Dolgol from './component/Dolgol';
+import HealthCheck from './component/HealthCheck';
+import Home from './component/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/api/v1/dolgol' element={<Dolgol />} />
+        <Route path='healthcheck' element={<HealthCheck />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
