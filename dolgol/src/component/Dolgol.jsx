2011cc96eecb14d4f6cc8d1faba28bdb8dc889c8
yeonju <llyyjj292@gmail.com>
2023-09-16 10:47:22 +0900

    feat: router component
    Signed-off-by: yeonju <llyyjj292@gmail.com>

---
+++ dolgol/src/component/Dolgol.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Dolgol() {
+  return (
+    <div>
+      <img src="https://t1.daumcdn.net/cfile/tistory/99862A4D5CF08BE818" alt="돌고래" />
+    </div>
+  )
+}
+
+export default Dolgol;
---
+++ dolgol/src/component/HealthCheck.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HealthCheck() {
+  return (
+    <div>
+      <h2>HEALTH GOOD !</h2>
+    </div>
+  )
+}
+
+export default HealthCheck;
